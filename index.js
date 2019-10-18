3e26bf3026e052833974c34128a0bdc03afcc9bc
Lucas <luksfreestyle_m_e_@hotmail.com>
2019-10-18 11:56:25 -0300

    Performance increased and easier access to state Increased update performance by adding a hash table with subscriptions added getFromPath as an easier access to state variables from outside hooks for better use of actions and side effects

--- index.js
+++ index.js
@@ -3,26 +3,50 @@ import dotProp from 'dot-prop-immutable';
 
 let gState = {};
 
+const subscriptionsHash = {};
+
 const sub = {
     subscriptions: [],
-    subscribe: function(keysAndPath, update) { this.subscriptions.push({keysAndPath, update}); return this.subscriptions.length - 1; },
-    unsubscribe: function(index) { this.subscriptions = this.subscriptions.filter((s, i) => i !== index) },
-    update: function(pathList) {
-        this.subscriptions.forEach(subscription => {
-            const shouldUpdate = pathList.some(key => subscription.keysAndPath.some(kp => kp[1] === key));
-
-            if(shouldUpdate) {
-                let update = {};
-                subscription.keysAndPath.forEach(kp => {
-                    update[kp[0]] = dotProp.get(gState, kp[1]);
-                })
-
-                pathList.forEach(function(key) {
-                    const name = subscription.keysAndPath.find(kp => kp[1] === key)[0];
-                    update[name] = dotProp.get(gState, key);
-                });
-                subscription.update(update);
+    subscribe: function(keysAndPath, update) {
+        this.subscriptions.push({keysAndPath, update});
+        keysAndPath.forEach(function(kp) {
+            const path = kp[1];
+            if(path) {
+                if(!subscriptionsHash[path]) {
+                    subscriptionsHash[path] = [];
+                }
+                subscriptionsHash[path].push({keysAndPath, update});
             }
+        });
+        return this.subscriptions.length - 1;
+    },
+    unsubscribe: function(index) {
+        const _sub = this.subscriptions[index];
+        _sub.keysAndPath.forEach(function(kp) {
+            subscriptionsHash[kp[1]] = subscriptionsHash[kp[1]].filter(function(sub2) {
+                return sub2.update !== _sub.update;
+            });
+            if(subscriptionsHash[kp[1]].length === 0) {
+                delete subscriptionsHash[kp[1]];
+            }
+        });
+        this.subscriptions = this.subscriptions.filter((s, i) => i !== index)
+    },
+    update: function(pathList) {
+        pathList.forEach(function(path) {
+            const subs = subscriptionsHash[path];
+            if(!subs) {
+                return;
+            }
+            subs.forEach(function(sub) {
+                let update = {};
+
+                sub.keysAndPath.forEach(kp => {
+                    update[kp[0]] = dotProp.get(gState, kp[1]);
+                });
+
+                sub.update(update);
+            })
         });
     }
 }
@@ -70,4 +94,8 @@ export function useSub(baseKey, keysAndPath) {
 
 export function getState() {
     return JSON.parse(JSON.stringify(gState));
+}
+
+export function getFromPath(path) {
+    return dotProp.get(gState, path);
 }
